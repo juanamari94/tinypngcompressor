833ef237d85e55871cce5fb6ba7dfc2245a2e1d8
Juan Amari <juan.jamari@gmail.com>
2017-01-27 12:30:35 -0300

    Added some output to provide feedback while compressing

--- tinypngcompressor.py
+++ tinypngcompressor.py
@@ -7,7 +7,7 @@ import subprocess
 
 tinify.key = "9x6LTmWtvaeaMcb3Bna5cwIxEctgP5Pv"
 
-def get_size_in_kilobytes(start_path = '.'):
+def get_size_in_kilobytes(start_path = '.', directory_list = None):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
         for f in filenames:
@@ -19,25 +19,41 @@ def get_size_in_kilobytes(start_path = '.'):
 
 def main():
     # Gotta validate that user input
-
     if len(sys.argv) == 2:
         source_dir = sys.argv[1]
-
         if not os.path.isdir(source_dir): # If source path doesn't exist.
             print("Source directory does not exist.") # Notify it.
         else:
             destination_dir = source_dir + "_tiny"
+            if os.path.isdir(destination_dir): # Delete the _tiny directory if it exists.
+                shutil.rmtree(destination_dir)
             shutil.copytree(source_dir, destination_dir)
             print("\nCompressing... This might take a while.")
             for root, subfolders, files in os.walk(destination_dir): #Walk the directory
                 for file in files:
-                    if file.endswith('.png'):
+                    if file.endswith(".png"):
                         source_full_path = root + "/" +file # Gotta get the full path
-                        tinify.from_file(source_full_path).to_file(source_full_path)
-            print("Done!\n")
-            print("Differences in directories:")
-            print("Uncompressed directory: " + str(get_size_in_kilobytes(source_dir)) + " KB")
-            print("Comrpessed directory: " + str(get_size_in_kilobytes(destination_dir)) + " KB")
+                        sys.stdout.write(".")
+                        sys.stdout.flush()
+                        tinify.from_file(source_full_path).to_file(source_full_path) # This will fail if you run out of compression calls to the TinyPNG API.
+            print("\nDone!")
+
+            # Parsing and assigning variables so that printing doesn't look like a complete mess
+            compressed_dir_size = get_size_in_kilobytes(source_dir)
+            uncompressed_dir_size = get_size_in_kilobytes(destination_dir)
+            size_difference_between_dirs = compressed_dir_size - uncompressed_dir_size
+            shortened_source_name = source_dir[source_dir.rfind('/')+1:len(source_dir)]
+            shortened_destination_name = destination_dir[destination_dir.rfind('/')+1:len(destination_dir)]
+            table_label = "Size in KB\tName"
+
+            display_list = ["\nSize in KB\tName",
+            str(compressed_dir_size) + "\t\t" + shortened_source_name,
+            str(uncompressed_dir_size) + "\t\t" + shortened_destination_name,
+            str(size_difference_between_dirs) + "\t\tDifference"]
+            maximum_length = len(max(display_list, key=len))
+            for label in display_list:
+                print(label)
+                print("-" * (maximum_length + 10))
     else:
         print("Invalid arguments.")
 
